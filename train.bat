1e6f93cf6c284a329976b2c503d12344bb395a3c
Kohei Matsuura <39566907+Kohei-Matsuura@users.noreply.github.com>
2020-08-25 18:21:46 +0900

    Update train.bat

--- train.bat
+++ train.bat
@@ -2,4 +2,4 @@
 
 mkdir /n/rd33/matsuura/save/${1}
 mkdir /n/rd33/matsuura/save/${1}/params
-gpujob python train.py '/n/rd33/matsuura/evals/KS.over256.htks' '/n/rd33/matsuura/evals/KM.over256.htks' /n/rd33/matsuura/save/${1} | tee /n/rd33/matsuura/save/${1}/loss.log
+gpujob python train.py '/n/rd33/matsuura/evals/cgan/KS.over128.htks' '/n/rd33/matsuura/evals/KM.over128.htks' /n/rd33/matsuura/save/${1} | tee /n/rd33/matsuura/save/${1}/loss.log
